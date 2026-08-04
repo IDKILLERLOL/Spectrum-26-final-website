353092ba8669342fc4139f467c5ec5f5b465ca23
Ishaan Doshi <i.doshi30@gmail.com>
2026-08-04 17:44:42 +0530

    Revert "Favicon: use white.ico for dark theme, black.ico for light theme; dynamically swap on switch"
    This reverts commit 2f2a5c72798223d0d0b9c4a5eaac731765dddb69.

--- src/main.tsx
+++ src/main.tsx
@@ -53,10 +53,6 @@ if (theme === 'dark') {
 } else {
   document.documentElement.classList.remove('dark');
 }
-const favicon = document.querySelector('link[rel="icon"]');
-if (favicon) {
-  favicon.setAttribute('href', theme === 'dark' ? '/white.ico' : '/black.ico');
-}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
