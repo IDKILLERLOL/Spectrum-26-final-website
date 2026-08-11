012f49b419ab34e03766f3d0f2a1bc09ef1de10d
Ishaan Doshi <i.doshi30@gmail.com>
2026-08-11 22:59:28 +0530

    fix(layout): wrap root HomePage in Shell and QuestProvider so top navbar is rendered on home screen

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
 import { getEvents } from "@/lib/server/firestore-events"
 import { HomePageClient } from "./(flagship)/HomePageClient"
+import { Shell } from "@/components/flagship/Shell"
+import { QuestProvider } from "@/components/flagship/quest-context"
+import { questBody } from "@/components/flagship/fonts"
 
 export default async function HomePage() {
   const events = await getEvents()
-  return <HomePageClient events={events} />
+  return (
+    <div className={questBody.className}>
+      <QuestProvider>
+        <Shell>
+          <HomePageClient events={events} />
+        </Shell>
+      </QuestProvider>
+    </div>
+  )
 }
