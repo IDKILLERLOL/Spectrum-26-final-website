11739d22ce3b44d46fd88245f303812d14efe5bf
Ishaan Doshi <i.doshi30@gmail.com>
2026-08-12 22:25:24 +0530

    Hello

--- app/supercore/(protected)/events/page.tsx
+++
@@ -1,7 +0,0 @@
-import { getEvents } from "@/lib/server/firestore-events"
-import { EventsAdminClient } from "./EventsAdminClient"
-
-export default async function AdminEventsPage() {
-  const events = await getEvents()
-  return <EventsAdminClient events={events} />
-}
--- app/supercore/(protected)/schedule/page.tsx
+++
@@ -1,7 +0,0 @@
-import { getSchedule } from "@/lib/server/firestore-schedule"
-import { ScheduleAdminClient } from "./ScheduleAdminClient"
-
-export default async function AdminSchedulePage() {
-  const schedule = await getSchedule()
-  return <ScheduleAdminClient schedule={schedule} />
-}
